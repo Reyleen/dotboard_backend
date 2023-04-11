b83441b014d9974aec30c19fef5a9011c33ec1a4
Reyleen <chell.delareyna@gmail.com>
2023-04-11 13:18:44 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/dotboard_backend/DotboardBackendApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.dotboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotboardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/uniroma3/siw/dotboard_backend/DotboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.dotboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
