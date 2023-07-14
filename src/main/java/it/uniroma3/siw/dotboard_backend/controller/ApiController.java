84f208e5662ce777e02ec6c597211dbf46500b1e
Francesco Gaudino <72342418+FrankGaudin@users.noreply.github.com>
2023-07-14 16:21:55 +0200

    Created apiService and fixed the controller and repository

--- src/main/java/it/uniroma3/siw/dotboard_backend/controller/ApiController.java
+++ src/main/java/it/uniroma3/siw/dotboard_backend/controller/ApiController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import it.uniroma3.siw.dotboard_backend.model.Api;
 import it.uniroma3.siw.dotboard_backend.model.BoardItem;
 import it.uniroma3.siw.dotboard_backend.repository.ApiRepository;
+import it.uniroma3.siw.dotboard_backend.services.ApiService;
 import it.uniroma3.siw.dotboard_backend.services.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,21 +19,22 @@ import java.util.Date;
 @SecurityRequirement(name = "Bearer_JWT")
 @Tag(name = "API", description = "The API. Has various functions and is placed inside the BoardItems.")
 public class ApiController implements Validator {
+
     @Autowired
-    private ApiRepository apiRepository;
+    private ApiService apiService;
 
 
     // GET all boardItems
     @Operation(summary = "Get all APIs of the system")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public Iterable<Api> getAll() {
-        return this.apiRepository.findAll();
+        return this.apiService.getAll();
     }
 
     @Operation(summary = "Get API by id")
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public Api getById(@PathVariable("id") Long id) {
-        return this.apiRepository.findById(id).orElse(null);
+        return this.apiService.getById(id);
     }
 
     //Da modificare ed implementare Api
@@ -40,23 +42,14 @@ public class ApiController implements Validator {
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "", method = RequestMethod.POST)
     public Api create(@RequestBody Api api) {
-        //dobbiamo collegarlo ad una api
-        return this.apiRepository.save(api);
+        return this.apiService.create(api);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Delete an API")
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") Long id) {
-        Api api = this.apiRepository.findById(id).orElse(null);
-        if (api == null) {
-            return;
-        }
-        for(BoardItem b : api.getBoarditems()) {
-            b.setApi(null);
-        }
-        api.setDeletedAt(new Date());
-        this.apiRepository.save(api);
+        this.apiService.delete(id);
     }
 
 }
---
+++ src/main/java/it/uniroma3/siw/dotboard_backend/services/ApiService.java
@@ -0,0 +1,47 @@
+package it.uniroma3.siw.dotboard_backend.services;
+
+import it.uniroma3.siw.dotboard_backend.model.Api;
+import it.uniroma3.siw.dotboard_backend.model.BoardItem;
+import it.uniroma3.siw.dotboard_backend.repository.ApiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Date;
+
+@Service
+public class ApiService {
+
+    @Autowired
+    private ApiRepository apiRepository;
+
+    @Transactional
+    public Iterable<Api> getAll() {
+        return this.apiRepository.findAll();
+    }
+
+    @Transactional
+    public Api getById(@PathVariable("id") Long id) {
+        return this.apiRepository.findByIdAndDeletedAtIsNull(id);
+    }
+
+    @Transactional
+    public Api create(@RequestBody Api api) {
+        return this.apiRepository.save(api);
+    }
+
+    @Transactional
+    public void delete(@PathVariable("id") Long id) {
+        Api api = this.getById(id);
+        if (api == null) {
+            return;
+        }
+        for(BoardItem b : api.getBoarditems()) {
+            b.setApi(null);
+        }
+        api.setDeletedAt(new Date());
+        this.apiRepository.save(api);
+    }
+}
